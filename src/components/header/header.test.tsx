6ec215e04794b269b1420567afbe43bbf5cc6880
Jhon Guerrero <garruxx@gmail.com>
2024-04-04 22:07:07 -0500

    header initializate

---
+++ src/components/header/header.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react'
+import { Header } from './header'
+
+describe('header', () => {
+	test('should render', () => {
+		const { baseElement } = render(<Header />)
+		expect(baseElement).toBeDefined()
+	})
+})
