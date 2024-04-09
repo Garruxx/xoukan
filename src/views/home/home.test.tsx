9a79752ab73bea09aef801f0e677d6ff9a5ee8f5
Jhon Guerrero <garruxx@gmail.com>
2024-04-09 01:45:23 -0500

    feat(components): :construction: button is created to copy the connection

--- src/views/home/home.test.tsx
+++ src/views/home/home.test.tsx
@@ -1,6 +1,9 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { Home } from './home'
 import { ui } from './lang/ui'
+import { act } from 'react-dom/test-utils'
+
+jest.useFakeTimers()
 describe('home', () => {
 	test('home should be defined', () => {
 		expect(Home).toBeDefined()
@@ -20,5 +23,80 @@ describe('home', () => {
 		const { getByText } = render(<Home ICEToCopy="" />)
 		expect(getByText(ui.welcome_text)).toBeDefined()
 		jest.resetAllMocks()
+	})
+
+	test('button to copy should be rendered', () => {
+		jest.spyOn(window.navigator, 'language', 'get').mockReturnValue('en-US')
+		const { getByRole } = render(<Home ICEToCopy="" />)
+		const button = getByRole('button')
+		expect(button).toBeDefined()
+		expect(button.textContent).toBe(ui.copy_conection_btn)
+		jest.resetAllMocks()
+	})
+	test('button to copy should be change their text when is copied', async () => {
+		//mocks
+		jest.spyOn(window.navigator, 'language', 'get').mockReturnValue('en-US')
+		const beforePrompt = window.prompt
+		window.prompt = jest.fn().mockReturnValue('test')
+
+		//renderer
+		const { getByRole } = render(<Home ICEToCopy="" />)
+		const button = getByRole('button')
+
+		// event
+		await act(async () => {
+			fireEvent.click(button)
+			await Promise.resolve()
+		})
+		//test
+		expect(button.textContent).toBe(ui.copied_connection)
+		// reset mocks
+		jest.resetAllMocks()
+		window.prompt = beforePrompt
+	})
+
+	test('button should return to its natural state 2s after being clicked.', async () => {
+		//mocks
+		jest.spyOn(window.navigator, 'language', 'get').mockReturnValue('en-US')
+		const beforePrompt = window.prompt
+		window.prompt = jest.fn().mockReturnValue('test')
+
+		//renderer
+		const { getByRole } = render(<Home ICEToCopy="" />)
+		const button = getByRole('button')
+
+		// event
+		await act(async () => {
+			fireEvent.click(button)
+			jest.runAllTimers()
+			await Promise.resolve()
+		})
+		//test
+		expect(button.textContent).toBe(ui.copy_conection_btn)
+		// reset mocks
+		jest.resetAllMocks()
+		window.prompt = beforePrompt
+	})
+
+	test('press the button should copy ICE', async () => {
+		//mocks
+		jest.spyOn(window.navigator, 'language', 'get').mockReturnValue('en-US')
+		const beforePrompt = window.prompt
+		window.prompt = jest.fn().mockReturnValue('test')
+
+		//renderer
+		const { getByRole } = render(<Home ICEToCopy="" />)
+		const button = getByRole('button')
+
+		// event
+		await act(async () => {
+			fireEvent.click(button)
+			await Promise.resolve()
+		})
+		//test
+		expect(window.prompt).toHaveBeenCalled()
+		// reset mocks
+		jest.resetAllMocks()
+		window.prompt = beforePrompt
 	})
 })
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,13 +1,24 @@
 import xoukan_logo from '@src/assets/xoukan.svg'
 import { ui } from './lang/ui'
 import home from './home.module.sass'
+import copy from 'copy-to-clipboard'
+import { useCallback, useState } from 'react'
 import { HomeProps } from './types/home.props'
+export const Home = ({ ICEToCopy }: HomeProps) => {
+	const [isCopied, setIsCopied] = useState(false)
+	const copyToClipboard = useCallback(() => {
+		copy(ICEToCopy)
+		setIsCopied(true)
+		setTimeout(() => setIsCopied(false), 2000)
+	}, [ICEToCopy])
 
-export const Home = ({}: HomeProps) => {
 	return (
 		<div className={home.home}>
 			<img src={xoukan_logo} alt="xoukan logo" width={222} height={222} />
 			<h2>{ui.welcome_text}</h2>
+			<button onClick={copyToClipboard} disabled={isCopied}>
+				{isCopied ? ui.copied_connection : ui.copy_conection_btn}
+			</button>
 		</div>
 	)
 }
