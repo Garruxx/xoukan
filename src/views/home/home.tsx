7d6317ca3e06dedfa94535c1aa17f331abfe04bc
Jhon Guerrero <garruxx@gmail.com>
2024-04-09 00:47:37 -0500

    feat(components): :construction: home styles starting

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,9 +1,10 @@
 import xoukan_logo from '@src/assets/xoukan.svg'
 import { ui } from './lang/ui'
+import home from './home.module.sass'
 
 export const Home = () => {
 	return (
-		<div>
+		<div className={home.home}>
 			<img src={xoukan_logo} alt="xoukan logo" width={222} height={222} />
 			<h2>{ui.welcome_text}</h2>
 		</div>
