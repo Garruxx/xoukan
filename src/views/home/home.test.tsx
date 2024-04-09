ba002edb77e87eb9417c8cb822131417a2673069
Jhon Guerrero <garruxx@gmail.com>
2024-04-08 22:23:59 -0500

    feat(components): :sparkles: home is rendered correctly

--- src/views/home/home.test.tsx
+++ src/views/home/home.test.tsx
@@ -1,6 +1,12 @@
+import { render } from '@testing-library/react'
 import { Home } from './home'
 describe('home', () => {
 	test('home should be defined', () => {
 		expect(Home).toBeDefined()
+	})
+
+	test('home should be render', () => {
+		const { baseElement } = render(<Home />)
+		expect(baseElement).toBeDefined()
 	})
 })
