41fadebd208cafcca5e4b202c1bb7320210d0ac5
Jhon Guerrero <garruxx@gmail.com>
2024-04-09 22:26:25 -0500

    feat(components): :construction: text field ice sets its value to useICE

--- src/views/home/components/text-field-ice/text-field-ice.test.tsx
+++ src/views/home/components/text-field-ice/text-field-ice.test.tsx
@@ -1,6 +1,8 @@
-import { render } from '@testing-library/react'
+import { act, fireEvent, render } from '@testing-library/react'
 import { TextFieldIce } from './text-field-ice'
 import { ui } from '../../lang/ui'
+import { ICEContextProvider } from '@src/context/ice/ice.context'
+import { useICE } from '@src/context/ice/hook/use-ice'
 describe('text-field-ICE', () => {
 	it('should render', () => {
 		expect(render(<TextFieldIce />)).toBeDefined()
@@ -14,5 +16,33 @@ describe('text-field-ICE', () => {
 	it('should render the input', () => {
 		const { getByRole } = render(<TextFieldIce />)
 		expect(getByRole('textbox')).toBeDefined()
+	})
+
+	it('should set the value to useICE', async () => {
+		// Test components
+		const TestComponentWithIce = () => {
+			const { ICE } = useICE()
+			return <span data-testid="ice-value">{ICE}</span>
+		}
+		const TestElement = () => {
+			return (
+				<ICEContextProvider>
+					<TestComponentWithIce />
+					<TextFieldIce />
+				</ICEContextProvider>
+			)
+		}
+
+		// get elements
+		const { getByTestId, getByRole } = render(<TestElement />)
+		const input = getByRole('textbox')
+		const iceValue = getByTestId('ice-value')
+
+		await act(async () => {
+			fireEvent.change(input, { target: { value: 'test value' } })
+			await Promise.resolve()
+		})
+		expect(input.getAttribute('value')).toBe('test value')
+		expect(iceValue.textContent).toBe('test value')
 	})
 })
--- src/views/home/components/text-field-ice/text-field-ice.tsx
+++ src/views/home/components/text-field-ice/text-field-ice.tsx
@@ -1,11 +1,25 @@
+import { useICE } from '@src/context/ice/hook/use-ice'
 import { ui } from '../../lang/ui'
+import { useCallback } from 'react'
 
 export const TextFieldIce = () => {
+	const { ICE, setICE } = useICE()
+	const handleChange = useCallback(
+		(e: React.ChangeEvent<HTMLInputElement>) => {
+			setICE(e.target.value)
+		},
+		[setICE],
+	)
 	return (
 		<div>
 			<label>
 				<span>{ui.input_label}</span>
-				<input type="text" placeholder={ui.input_placeholder} />
+				<input
+					type="text"
+					placeholder={ui.input_placeholder}
+					value={ICE}
+					onChange={handleChange}
+				/>
 			</label>
 		</div>
 	)
