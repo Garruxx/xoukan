62d15a28823b740dc4bb01e4c3dbd260acd6adb4
Jhon Guerrero <garruxx@gmail.com>
2024-04-16 22:29:17 -0500

    refactor(webRTC): :recycle: webRTC is move to web-rtc/connection

--- src/web-rtc/connection/web-rtc-connect.ts
+++ src/web-rtc/connection/web-rtc-connect.ts
@@ -16,7 +16,7 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-import { iceServers } from './ice-servers'
+import { iceServers } from '../config/ice-servers'
 export class WRTCConnect extends RTCPeerConnection {
 	public channels: Map<string, RTCDataChannel> = new Map()
 	private candidates: RTCIceCandidate[] = []
