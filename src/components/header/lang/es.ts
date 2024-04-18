c7c298bdba8579f15e8afdd39ce9511545c5c9eb
Jhon Guerrero <garruxx@gmail.com>
2024-04-18 10:31:38 -0500

    refactor(lang): :recycle: the offlineInfoText now is a simple string

--- src/components/header/lang/es.ts
+++ src/components/header/lang/es.ts
@@ -1,11 +1,10 @@
 export const ui_es = {
 	online: 'En linea ⚡',
 	offline: 'Desconectado 🌧',
-	offlineInfoText: `
-		Cuando el otro usuario inicia la conexión, pega su enlace
-		en el campo de texto y envía tu respuesta. Si no, copia tu 
-		enlace, envíalo y espera su respuesta para pegarla en el campo de texto.
-	`,
+	offlineInfoText:
+		'Cuando el otro usuario inicia la conexión, pega su enlace ' +
+		'en el campo de texto y envía tu respuesta. Si no, copia tu ' +
+		'enlace, envíalo y espera su respuesta para pegarla en el campo de texto.',
 	onlineInfoText:
 		'⚡ Estás conectado ⚡ ahora solo comparte y recibe archivos',
 }
