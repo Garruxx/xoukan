4ef6e026faab37059330e04596f405927f2f2bf9
Jhon Guerrero <garruxx@gmail.com>
2024-04-18 09:54:16 -0500

    feat(lang): :globe_with_meridians: uiText create for header

---
+++ src/components/header/lang/en.ts
@@ -0,0 +1,9 @@
+export const ui_en = {
+	online: 'Online ⚡',
+	offline: 'Offline 🌧',
+	offlineInfoText: `
+		When the other user initiates the connection, paste their link in the text field and send your response. Otherwise, copy your link,
+		send it, and wait for their response to paste it in the text field.
+	`,
+	onlineInfoText: `⚡ You are online ⚡ now only share and receive files`,
+}
