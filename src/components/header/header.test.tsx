83cf0089b1a1c8353e2bbd8eb7cbbb077d51fe79
Jhon Guerrero <garruxx@gmail.com>
2024-04-18 10:32:35 -0500

    test(components): :white_check_mark: header tests update

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,32 +1,35 @@
 import { render } from '@testing-library/react'
 import { Header } from './header'
+import { ui } from './lang/ui'
 
 describe('header', () => {
 	test('should render', () => {
-		const { baseElement } = render(
-			<Header conectionStatus="" infoText="" />,
-		)
+		const { baseElement } = render(<Header isConnected />)
 		expect(baseElement).toBeDefined()
 	})
 
-	test('The connection status must be rendered', () => {
-		const { getByText } = render(
-			<Header conectionStatus="online" infoText="" />,
-		)
-		expect(getByText('online')).toBeDefined()
+	test('The connection status online must be rendered when is connected', () => {
+		const { getByText } = render(<Header isConnected={true} />)
+		expect(getByText(ui.online)).toBeDefined()
 	})
-	test('The infoText must be rendered', () => {
-		const { getByText } = render(
-			<Header conectionStatus="" infoText="infoText test" />,
-		)
-		expect(getByText('infoText test')).toBeDefined()
+	test('The onlineInfoText must be rendered when is connected', () => {
+		const { getByText } = render(<Header isConnected />)
+		expect(getByText(ui.onlineInfoText)).toBeDefined()
+	})
+	test('The connection status ofline must be rendered when is disconnected', () => {
+		const { getByText } = render(<Header isConnected={false} />)
+		expect(getByText(ui.offline)).toBeDefined()
+	})
+	test('The offlineInfoText must be rendered when is disconnected', () => {
+		const { getByText } = render(<Header isConnected={false} />)
+		expect(getByText(ui.offlineInfoText)).toBeDefined()
 	})
 	test('The git logo must be rendered', () => {
-		const { getByRole } = render(<Header conectionStatus="" infoText="" />)
+		const { getByRole } = render(<Header isConnected />)
 		expect(getByRole('img')).toBeDefined()
 	})
 	test('The link to the repository must be rendered', () => {
-		const { getByRole } = render(<Header conectionStatus="" infoText="" />)
+		const { getByRole } = render(<Header isConnected />)
 		const anchor = getByRole('link')
 		expect(anchor).toBeDefined()
 		expect(anchor.getAttribute('href')).toBe(
@@ -35,9 +38,7 @@ describe('header', () => {
 	})
 
 	test('Should renderer correctly', () => {
-		const { baseElement } = render(
-			<Header conectionStatus="" infoText="infoText test" />,
-		)
+		const { baseElement } = render(<Header isConnected />)
 		expect(baseElement.innerHTML).toMatchSnapshot()
 	})
 })
