eb036f0a2fe3d0554a7903dcc076378d4377434f
Jhon Guerrero <garruxx@gmail.com>
2024-04-18 09:20:57 -0500

    feat(webRTC): :sparkles: simple-rtc-connections exports your parts

---
+++ src/web-rtc/simple-rtc-connection/index.ts
@@ -0,0 +1,8 @@
+import { getSignal } from './get-signal/get-signal'
+import { setSignal } from './set-signal/set-signal'
+
+export const simpleRTCConnection = {
+	getSignal,
+	setSignal,
+}
+export default simpleRTCConnection
