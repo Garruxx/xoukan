49bc4f5c8bc857a2999ad7b1cd8cdefde03d73f9
Jhon Guerrero <garruxx@gmail.com>
2024-04-24 12:36:34 -0500

    feat(components): :construction: starting the transfer case

---
+++ src/views/transferer/components/transfer-case/transfer-case.tsx
@@ -0,0 +1,8 @@
+import transferCase from './transfer-case.module.sass'
+export const TransferCase = () => {
+	return (
+		<div className={transferCase.transferCase}>
+			<h1>Transfer Case</h1>
+		</div>
+	)
+}
